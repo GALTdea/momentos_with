41356ec512781b612fcc066407855a1cd0adab95
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2024-02-26 10:05:40 -0800

    Generate promt model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_26_175837) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_26_180503) do
   create_table "children", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -21,6 +21,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_26_175837) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_children_on_user_id"
+  end
+
+  create_table "prompts", force: :cascade do |t|
+    t.text "question"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
