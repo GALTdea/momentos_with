598f7ea7b03f1efb5bb7ffe13602788c1e01f43d
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2024-03-01 16:43:49 -0800

    change redirect_to param root_path to user_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   private
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
-    redirect_to(request.referrer || root_path)
+    redirect_to(request.referrer || user_path(current_user))
   end
 end
