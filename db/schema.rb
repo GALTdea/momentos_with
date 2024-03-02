d1f8c960b34da03a30eade20ed56ee069ff1f848
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2024-03-01 16:15:35 -0800

    added role col to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_26_181023) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_02_001318) do
   create_table "children", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_26_181023) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
