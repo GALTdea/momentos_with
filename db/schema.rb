a534a573daf51a169bbd03b5727054d9d91d8402
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2024-03-05 13:20:03 -0800

    migrate quiz model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_05_194649) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_05_211150) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -82,6 +82,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_05_194649) do
     t.datetime "updated_at", null: false
     t.string "category"
     t.string "type"
+  end
+
+  create_table "quizzes", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "total_questions"
+    t.string "category"
+    t.string "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
