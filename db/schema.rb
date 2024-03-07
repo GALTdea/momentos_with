1c2a15464530426963a71ac8b6f50cff3e443821
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2024-03-07 14:25:46 -0800

    Creates Answer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_07_222202) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_07_222502) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -47,6 +47,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_07_222202) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "answers", force: :cascade do |t|
+    t.string "content"
+    t.integer "question_id", null: false
+    t.integer "quiz_session_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["quiz_session_id"], name: "index_answers_on_quiz_session_id"
   end
 
   create_table "children", force: :cascade do |t|
@@ -134,6 +144,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_07_222202) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "quiz_sessions"
   add_foreign_key "children", "users"
   add_foreign_key "entries", "children"
   add_foreign_key "entries", "prompts"
