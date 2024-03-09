bbb416bc03350f939c70cf1665570934a89bfb2e
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2024-03-09 12:02:48 -0800

    Add logic to new update aswell as set quiz and quiz_params

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,4 +1,6 @@
 class QuizzesController < ApplicationController
+  before_action :set_quiz, only: [:show, :edit, :update]
+
   def index
   end
 
@@ -6,8 +8,34 @@ class QuizzesController < ApplicationController
   end
 
   def new
+    @quiz = Quiz.new
+  end
+
+  def create
+    @quiz = Quiz.new(quiz_params)
+    respond_to do |format|
+      if @quiz.save
+        redirect_to @quiz
+      else
+        render :new
+      end
+    end
   end
 
   def edit
   end
+
+  def update
+  end
+
+  private
+
+  def set_quiz
+    @quiz = Quiz.find(params[:id])
+  end
+
+  def quiz_params
+    params.require(:quiz).permit(:title, :description)
+  end
+
 end
