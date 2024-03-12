ed1588127224f8fb4b441bf07ab9d3758fdf7a43
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2024-03-12 12:17:30 -0700

    Add questions controller create action code to include turbo

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,6 +8,27 @@ class QuestionsController < ApplicationController
   def new
   end
 
+  def create
+    @quiz = Quiz.find(params[:quiz_id])
+    @question = @quiz.questions.build(question_params)
+
+    respond_to do |format|
+      if @question.save
+        format.turbo_stream
+        format.html { redirect_to @quiz }
+      else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("new_question", partial: "questions/form", locals: { quiz: @quiz, question: @question }) }
+        format.html { render 'quizzes/show', status: :unprocessable_entity }
+      end
+    end
+  end
+
   def edit
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:text)
   end
 end
