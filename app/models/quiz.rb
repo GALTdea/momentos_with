53c9322a886d171b5adad909782dc62bee3a8c9b
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2024-03-16 13:36:41 -0700

    Add has_many quiz_session relationship to quiz model

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,4 +1,5 @@
 class Quiz < ApplicationRecord
   has_many :questions, dependent: :destroy
+  has_many :quiz_sessions, dependent: :destroy
   accepts_nested_attributes_for :questions, allow_destroy: true
 end
