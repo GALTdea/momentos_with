e9f418fd3191b677662a2ac05afbe21dc2596624
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2024-03-19 09:41:29 -0700

    Add next_question method to quiz_sessions

--- app/models/quiz_session.rb
+++ app/models/quiz_session.rb
@@ -2,4 +2,11 @@ class QuizSession < ApplicationRecord
   belongs_to :child
   belongs_to :quiz
   has_many :answers
+
+
+  def next_question
+    # Simple example: Get the next question by sequence that hasn't been answered yet
+    answered_question_ids = self.answers.pluck(:question_id)
+    self.quiz.questions.where.not(id: answered_question_ids).order(:sequence_number).first
+  end
 end
