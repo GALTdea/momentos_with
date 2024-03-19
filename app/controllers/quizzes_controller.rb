cf77ec5de4610a7ae21610d2f7d80491aaff6a67
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2024-03-19 11:56:37 -0700

    Add add question method to quizzes controller

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -32,6 +32,18 @@ class QuizzesController < ApplicationController
     redirect_to quizzes_url, notice: 'Quiz was successfully destroyed.'
   end
 
+  def add_questions
+    @quiz = Quiz.find(params[:id])
+    @quiz.question_ids = params[:quiz][:question_ids]
+
+    if @quiz.save
+      # Handle success (perhaps redirect or re-render the component to show the updated list of questions)
+    else
+      # Handle failure
+    end
+  end
+
+
   private
 
   def set_quiz
