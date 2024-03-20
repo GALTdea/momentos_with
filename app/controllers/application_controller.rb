a2698dbb49288b0eb0d3c0a98d394da90961179f
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2024-03-20 14:38:40 -0700

    Add pundit user_not_authoirzed method and callback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
-  include Pundit::Authorization
+  include Pundit
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  after_action :verify_authorized
+  after_action :verify_policy_scoped
   private
+
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
-    redirect_to(request.referrer || user_path(current_user))
+    redirect_to(request.referrer || root_path)
   end
 end
