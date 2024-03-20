a6a40b694dbeff3660abf66712af22035a5892a4
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2024-03-20 10:54:13 -0700

    Adds enume for role attribute in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :children
+
+  enum role: [:user, :admin]
 end
