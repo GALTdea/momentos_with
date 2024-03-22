2786b0c69eb0cb3bb12383495459aa5968839dd4
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2024-03-22 11:05:30 -0700

    Add accepts_nested_attributes_for child in user  model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :children
 
+  accepts_nested_attributes_for :children, allow_destroy: true
+
   enum role: [:user, :admin]
 end
