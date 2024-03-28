f38f97099c6059e8a7d2e31250c7850c8618ab84
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2024-03-28 10:57:49 -0700

    Redirects to start action in quiz_sessions after answer is created, passes quiz_session id params

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -9,22 +9,17 @@ class AnswersController < ApplicationController
       next_question = @answer.quiz_session.next_question
 
       if next_question
-        # Redirect to the next question's show page
-        redirect_to question_path(next_question), notice: 'Your answer was successfully recorded.'
+        redirect_to action: :start, controller: 'quiz_sessions', id: @answer.quiz_session_id, notice: 'Your answer was successfully recorded.'
       else
-        # Handle end of quiz, e.g., show results or a completion message
         redirect_to quiz_completion_path(@answer.quiz_session), notice: 'Quiz completed!'
       end
     else
-      # Handle the error, perhaps by rendering the current question again with an error message
       render 'questions/show', status: :unprocessable_entity
     end
   end
 
   private
-
   def answer_params
     params.require(:answer).permit(:options, :question_id, :quiz_session_id, :text, :user_id, :selected_option)
-    # Ensure these parameters match what your form is sending
   end
 end
